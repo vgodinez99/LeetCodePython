199b619427c605dc38610e9ab1a17f705b0686e7
Víctor Godínez <vgodinez99_IDK@outlook.com>
2025-09-04 17:16:44 -0600

    Update 13.RomantoInteger.py

--- 13.RomantoInteger.py
+++ 13.RomantoInteger.py
@@ -3,16 +3,17 @@ class Solution:
         
         self.s = s
 
-        dic = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000,}
+        dic = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000,"IV":4,"IX":9,"XL":40,"XC":90,"CD":400,"CM":900}
         sum = 0
+        i = 0 
 
-        for i in range(len(s)):
-            if s[i] == "I":
-                if s[i+1] = "V":
-                    
-
-            sum = sum + dic[s[i]]
-
+        while i < len(s):
+            if i + 1 < len(s) and s[i:i+2] in dic:
+                sum += dic[s[i:i+2]]
+                i += 2
+            else:
+                sum += dic[s[i]]
+                i += 1
         return sum
     
 s = 'MCMXCIV'
