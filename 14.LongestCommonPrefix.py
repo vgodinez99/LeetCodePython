6aa071b388db8028b41aa940ad718bfb296a462d
Víctor Godínez <vgodinez99_IDK@outlook.com>
2025-09-04 17:23:01 -0600

    Add the problem 14
    14. Longest Common Prefix

---
+++ 14.LongestCommonPrefix.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        return strs
+    
+strs = ["flower","flow","flight"]
+
+sol = Solution()
+s = sol.longestCommonPrefix(strs)
+
+print(s)
